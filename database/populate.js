072949cc5f1880265c27607a134a995c6dcbd627
Krzysztof Pawelec <ScriptyChris@gmail.com>
2020-12-04 23:09:57 +0100

    Extend script to normalize grouping categories; Fix param checking;

--- database/populate.js
+++ database/populate.js
@@ -3,7 +3,14 @@ const productSchema = require('./schemas/product');
 const { sep } = require('path');
 const { promisify } = require('util');
 const glob = promisify(require('glob'));
-const readFile = promisify(require('fs').readFile);
+
+let { readFile, readFileSync } = require('fs');
+readFile = promisify(readFile);
+
+const PARAMS = {
+  CLEAN_ALL: 'cleanAll=true',
+  GROUP_CATEGORIES: 'categoriesGroupPath='
+};
 
 console.log('process.argv', process.argv);
 
@@ -11,13 +18,12 @@ console.log('process.argv', process.argv);
   await connectToDB();
   const Product = getProductModel();
 
-  if (shouldCleanAllProductsBeforePopulation()) {
-    const removedProducts = await Product.remove();
+  if (getScriptParamValue(PARAMS.CLEAN_ALL)) {
+    const removedProducts = await Product.deleteMany();
     console.log(`Cleaning done - removed ${removedProducts.deletedCount} products.`);
   }
 
   const sourceDataList = await getSourceData();
-  // console.log('sourceData', sourceDataList);
 
   await populateProducts(Product, sourceDataList);
 
@@ -48,52 +54,82 @@ async function getSourceData() {
   const fileName = process.argv[3];
   const isFileInPath = path.endsWith('.json');
 
-  if (!isFileInPath && !fileName) {
+  if (!isFileInPath && (!fileName || fileName === PARAMS.CLEAN_ALL)) {
     return Promise.reject('If file name is not included in path as first argument, it must be provided separately - as second argument!');
   }
 
   const sourceDataPath = isFileInPath ? path : `${path}${sep}**${sep}${fileName}.json`;
-  const sourceDataFiles = (await glob(sourceDataPath)/**/).slice(22, 33);
+  const sourceDataFiles = await glob(sourceDataPath);
   const sourceDataList = await Promise.all(
       sourceDataFiles.map(async filePath => JSON.parse(await readFile(filePath, { encoding: 'utf8' })))
   );
 
+  console.log('Got sourceDataList from sourceDataPath:', sourceDataPath);
+
   return sourceDataList;
 }
 
-function shouldCleanAllProductsBeforePopulation() {
-  return process.argv.includes('cleanAll=true');
-}
-
 async function populateProducts(ProductModel, sourceDataList) {
+  const normalizersObj = {
+    category: categoryNameGrouper()
+  }
+
   return Promise.all(
       sourceDataList.map((data) => {
-        const product = new ProductModel(normalizeData(data));
+        const product = new ProductModel(normalizeData(data, normalizersObj));
         return product.save();
       })
   );
 
-  function normalizeData(data) {
+  function normalizeData(data, normalizersObj) {
+    const categoryName = data.nameAndCategory.category.name;
+
     const normalizedData = {
       ...data,
       name: data.nameAndCategory.name,
-      category: data.nameAndCategory.category.name,
+      category: normalizersObj.category(categoryName)
     };
 
     delete normalizedData.nameAndCategory;
 
-    if (data.reviews[0] === null) {
-      normalizedData.reviews = {
-        summary: {},
-        list: []
-      }
-    } else {
-      normalizedData.reviews = {
-        summary: data.reviews[0],
-        list: data.reviews.slice(1)
-      }
-    }
+    const isAnyReview = data.reviews[0] !== null;
+
+    normalizedData.reviews = {
+      summary: isAnyReview ? data.reviews[0] : {},
+      list: isAnyReview ? data.reviews.slice(1) : []
+    };
 
     return normalizedData;
   }
+
+  function categoryNameGrouper() {
+    const categoriesGroupPath = getScriptParamValue(PARAMS.GROUP_CATEGORIES, true);
+    const categoryRegExps = JSON.parse(readFileSync(categoriesGroupPath.split('=').pop(), { encoding: 'utf8' }));
+
+    if (categoriesGroupPath) {
+        return (categoryName) => {
+          const matchedRegExp = categoryRegExps
+            .find(regExp => typeof regExp.matcher === 'string' && (new RegExp(regExp.matcher)).test(categoryName));
+
+          if (matchedRegExp) {
+            return categoryName.replace(new RegExp(matchedRegExp.matcher), matchedRegExp.replacer);
+          }
+
+          return categoryName;
+        };
+    }
+
+    return (categoryName) => categoryName;
+  }
+}
+
+function getScriptParamValue(param, lenientSearch) {
+  return process.argv
+    .find((arg) => {
+      if (lenientSearch) {
+        return arg.includes(param);
+      }
+
+      return arg === param;
+  });
 }
