a67e17d19e5fdbb6b07c23878e6f082fa1a94a47
Krzysztof Pawelec <ScriptyChris@gmail.com>
2020-12-30 18:16:35 +0100

    [WIP] implementing categories tree multi select;

--- src/components/views/categoriesTree.js
+++ src/components/views/categoriesTree.js
@@ -1,9 +1,12 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, createRef } from 'react';
 import apiService from '../../features/apiService';
 import TreeMenu from 'react-simple-tree-menu';
 
 export default function CategoriesTree() {
   const [categoriesMap, setCategoriesMap] = useState(null);
+  const categoriesTreeRef = createRef();
+  const treeMenuRef = createRef();
+  const activeTreeNodes = new Map();
 
   useEffect(() => {
     (async () => {
@@ -17,7 +20,17 @@ export default function CategoriesTree() {
     if (categoriesMap) {
       const treeData = categoriesMap.map(getCategoriesTree.recursiveMapper);
 
-      return <TreeMenu data={treeData} />;
+      return (
+        <TreeMenu
+          data={treeData}
+          onClickItem={(clickedItem) => {
+            // console.log('clickedItem:', clickedItem);
+
+            toggleActiveTreeNode(clickedItem.level, clickedItem.index, clickedItem.label);
+          }}
+          ref={treeMenuRef}
+        />
+      );
     }
 
     return null;
@@ -35,6 +48,7 @@ export default function CategoriesTree() {
     if (isNestedCategory) {
       return {
         key,
+        index,
         label: categoryItem.parentCategory,
         nodes: categoryItem.childCategories.map((item, idx, __) =>
           getCategoriesTree.recursiveMapper(item, idx, __, level + 1)
@@ -44,14 +58,56 @@ export default function CategoriesTree() {
 
     return {
       key,
+      index,
       label: categoryItem,
     };
   };
 
+  const toggleActiveTreeNode = (nodeLevel, nodeIndex, nodeLabel) => {
+    const currentNodeKey = `${nodeLevel}-${nodeIndex}`;
+    const isActiveTreeNode /*activeTreeNodeIndex*/ = activeTreeNodes.has(currentNodeKey); //.findIndex(({ level, index }) => level === nodeLevel && index === nodeIndex);
+
+    if (isActiveTreeNode /*activeTreeNodeIndex*/ /* > -1*/) {
+      activeTreeNodes.delete(currentNodeKey); //.splice(activeTreeNodeIndex, 1);
+    } else {
+      activeTreeNodes.set(currentNodeKey, { nodeLevel, nodeIndex, nodeLabel }); //.push({ nodeLevel, nodeIndex, nodeLabel });
+    }
+
+    // This is a dirty workaround, because 3rd-party TreeMenu component doesn't seem to support multi selection.
+    activeTreeNodes.forEach((label, key) => {
+      const [level, index] = key.split('-');
+      const treeNodeLevelSelector = `.rstm-tree-item-level${level}`;
+      const treeNodeDOM = categoriesTreeRef.current.querySelectorAll(treeNodeLevelSelector)[index];
+
+      // "Force" DOM actions execution on elements controlled by React.
+      requestAnimationFrame(() => {
+        treeNodeDOM.classList.add('rstm-tree-item--active');
+        treeNodeDOM.setAttribute('aria-pressed', true);
+
+        // console.log('treeNodeDOM:', treeNodeDOM, ' /classList:', treeNodeDOM.classList);
+      });
+    });
+
+    // treeMenuRef.current.updater.enqueueSetState(treeMenuRef.current, { activeKey: '' }, (...args) => {
+    //   console.log('[updated state] args:', args);
+    //
+    //   treeMenuRef.current.updater.enqueueForceUpdate(treeMenuRef.current, (...args) =>
+    //     console.log('[rerendered] args:', args)
+    //   );
+    // });
+
+    console.warn('activeTreeNodes:', [...activeTreeNodes], ' /treeMenuRef:', treeMenuRef.current);
+  };
+
   return (
-    <>
+    /*
+      Attribute [ref] is used on whole component wrapper, because
+      both useRef() hook and React.createRef() method don't seem to
+      give reference to nested functional component's DOM elements, such as used TreeMenu.
+    */
+    <div ref={categoriesTreeRef}>
       [Categories Tree...]
       {getCategoriesTree()}
-    </>
+    </div>
   );
 }
