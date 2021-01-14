160a3066a75c78887d8b60bb3713c2f8fe56e672
Krzysztof Pawelec <ScriptyChris@gmail.com>
2021-01-14 12:55:19 +0100

    [WIP] group Prettier processing (done during running unit tests) results in array to show which files were formatted;

--- jestWatchPlugin.js
+++ jestWatchPlugin.js
@@ -1,22 +1,38 @@
-const { execSync, spawnSync } = require('child_process');
+const { execSync, spawnSync, spawn } = require('child_process');
 const { normalize } = require('path');
 const NORMALIZED_PRETTIER_BIN_PATH = normalize('node_modules/prettier/bin-prettier.js');
 
 var testCommand = 'node node_modules/prettier/bin-prettier.js --write E:/Projects/Fake-PEV-Shopping/test/database/utils/queryBuilder.spec.js --check';
 
 module.exports = class JestWatchPlugin {
+  constructor() {
+    this.formattedTestFiles = [];
+  }
+
   apply(jestHooks) {
     jestHooks.onFileChange(({ projects }) => {
       console.log('--- jest FILE CHANGED');
 
       // executeAndWait(() => {
-      setTimeout(() => {
+      // setTimeout(() => {
         console.log('--- jest CALLBACK');
 
-        // var spawnRes = spawnSync(testCommand.split(' ')[0], testCommand.split(' ').slice(1), { stdio: 'inherit' });
-        // console.log('---==== spawnRes:', spawnRes);
+        // var spawnRes = spawn(testCommand.split(' ')[0], testCommand.split(' ').slice(1)/*, { stdio: 'inherit' }*/);
+        var spawnRes = spawnSync(testCommand.split(' ')[0], testCommand.split(' ').slice(1)/*, { stdio: 'inherit' }*/);
 
-        const result = execSync(testCommand, { stdio: 'inherit' });
+        setTimeout(() => {
+          console.log('---==== spawnRes:', spawnRes.output.toString());
+        }, 2000);
+        this.formattedTestFiles.push(spawnRes.output.toString().trim().split('\n'));
+
+        // spawnRes.stdout.on('data', (data) => {
+        //   setTimeout(() => {
+        //     console.log(`>>> stdout: ${data.toString()}`);
+        //   }, 2000);
+        //     this.formattedTestFiles.push(data.toString().trim().split('\n'));
+        // });
+
+        // const result = execSync(testCommand, { stdio: 'inherit' });
 
         // const { testPaths } = projects[0];
         // console.log('--- watch plugin /testPaths:', testPaths);
@@ -25,10 +41,13 @@ module.exports = class JestWatchPlugin {
         // console.log('--- filePaths:',filePaths);
         //
         // execSync(`node ${NORMALIZED_PRETTIER_BIN_PATH} --write ${filePaths}`);
+    });
 
-        console.log('--- jest CALLBACK END ---\n', (result || '').toString());
-      }, 0);
-    })
+    jestHooks.onTestRunComplete(() => {
+      setTimeout(() => {
+        console.log('this.formattedTestFiles:', this.formattedTestFiles);
+      }, 3000);
+    });
   }
 }
 
