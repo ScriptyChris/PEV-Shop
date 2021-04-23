313c1295a69a35e3c3434c2e4bf57a1c1d03e46e
Krzysztof Pawelec <ScriptyChris@gmail.com>
2021-04-23 11:05:24 +0200

    Prettify code;

--- src/middleware/dev-proxy/standalone-proxy.ts
+++ src/middleware/dev-proxy/standalone-proxy.ts
@@ -12,22 +12,31 @@ const url = process.argv[3] || '/dev-proxy';
 const target = process.argv[4] || 'https://secure.snd.payu.com/api/v2_1/orders';
 const app: Application = Express();
 
-app.use(proxy(url, {
+app.use(
+  proxy(url, {
     target: target,
     changeOrigin: true,
     ignorePath: true,
     logLevel: 'debug',
     onError(err: unknown, req: Request, res: Response) {
-        debugger;
-        logger.log('[onError] err:', err, ' /req:', req, ' /res:', res);
+      // debugger;
+      logger.log('[onError] err:', err, ' /req:', req, ' /res:', res);
     },
     onProxyReq(proxyReq: ClientRequest, req: Request, res: Response) {
-        debugger;
-        logger.log('[onProxyReq] proxyReq.path:', proxyReq.path, ' /headers:', proxyReq.getHeaders());
+      // debugger;
+      logger.log('[onProxyReq] proxyReq.path:', proxyReq.path, ' /headers:', proxyReq.getHeaders());
     },
     onProxyRes(proxyRes: IncomingMessage, req: Request, res: Response) {
-        debugger;
-        logger.log('[onProxyRes] proxyRes.statusCode:', proxyRes.statusCode, ' /statusMessage:', proxyRes.statusMessage, ' /proxyRes.headers:', proxyRes.headers);
+      // debugger;
+      logger.log(
+        '[onProxyRes] proxyRes.statusCode:',
+        proxyRes.statusCode,
+        ' /statusMessage:',
+        proxyRes.statusMessage,
+        ' /proxyRes.headers:',
+        proxyRes.headers
+      );
     },
-}));
+  })
+);
 app.listen(port, () => logger.log('Proxy server is listening on port:', port));
