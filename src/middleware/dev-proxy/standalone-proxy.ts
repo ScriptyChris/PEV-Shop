b8110631db6692f46239c52858322d0fadcaf486
Krzysztof Pawelec <ScriptyChris@gmail.com>
2021-04-23 10:15:17 +0200

    Fix connection issues with proxy to PayU API;

--- src/middleware/dev-proxy/standalone-proxy.ts
+++ src/middleware/dev-proxy/standalone-proxy.ts
@@ -1,9 +1,10 @@
 // @ts-ignore
-import Express from 'express';
+import Express, { Request, Response } from 'express';
 import { Application } from 'express';
 // @ts-ignore
 import proxy from '../../../../node_modules/webpack-dev-server/node_modules/http-proxy-middleware';
 import getLogger from '../../../utils/logger';
+import { ClientRequest, IncomingMessage } from 'http';
 
 const logger = getLogger(module.filename);
 const port = Number(process.argv[2]) || 3001;
@@ -11,5 +12,22 @@ const url = process.argv[3] || '/dev-proxy';
 const target = process.argv[4] || 'https://secure.snd.payu.com/api/v2_1/orders';
 const app: Application = Express();
 
-app.use(proxy(url, { target, changeOrigin: true }));
+app.use(proxy(url, {
+    target: target,
+    changeOrigin: true,
+    ignorePath: true,
+    logLevel: 'debug',
+    onError(err: unknown, req: Request, res: Response) {
+        debugger;
+        logger.log('[onError] err:', err, ' /req:', req, ' /res:', res);
+    },
+    onProxyReq(proxyReq: ClientRequest, req: Request, res: Response) {
+        debugger;
+        logger.log('[onProxyReq] proxyReq.path:', proxyReq.path, ' /headers:', proxyReq.getHeaders());
+    },
+    onProxyRes(proxyRes: IncomingMessage, req: Request, res: Response) {
+        debugger;
+        logger.log('[onProxyRes] proxyRes.statusCode:', proxyRes.statusCode, ' /statusMessage:', proxyRes.statusMessage, ' /proxyRes.headers:', proxyRes.headers);
+    },
+}));
 app.listen(port, () => logger.log('Proxy server is listening on port:', port));
--- src/middleware/routes/api-orders.ts
+++ src/middleware/routes/api-orders.ts
@@ -1,11 +1,11 @@
 import getLogger from '../../../utils/logger';
 import { Request, Response } from 'express';
 import * as expressModule from 'express';
-import fetch, { FetchError, Response as FetchResponse } from 'node-fetch';
+import fetch, { FetchError, RequestInit, Response as FetchResponse } from 'node-fetch';
 
 const {
-    // @ts-ignore
-    default: { Router },
+  // @ts-ignore
+  default: { Router },
 } = expressModule;
 const router = Router();
 const logger = getLogger(module.filename);
@@ -13,133 +13,81 @@ const logger = getLogger(module.filename);
 logger.log('api-orders....');
 
 enum PAYMENT_URL {
-    VPS = 'http://pev-demo.store:3001/dev-proxy',
-    PAY_U = 'https://secure.snd.payu.com/api/v2_1/orders',
+  VPS = 'http://pev-demo.store:3001/dev-proxy',
+  PAY_U = 'https://secure.snd.payu.com/api/v2_1/orders',
 }
 
 router.options('/api/orders', (req: Request, res: Response) => {
-    logger.log('OPTIONS /api/orders:', req.headers);
+  logger.log('OPTIONS /api/orders:', req.headers);
 
-    res.setHeader('Access-Control-Allow-Headers','authorization,content,content-type');
-    res.setHeader('Access-Control-Allow-Methods', 'POST');
+  res.setHeader('Access-Control-Allow-Headers', 'authorization,content,content-type');
+  res.setHeader('Access-Control-Allow-Methods', 'POST');
 
-    res.send();
+  res.send();
 });
 
 router.post('/api/orders', (req: Request, res: Response) => {
-    logger.log('POST /api/orders env:', process.env.NODE_ENV);
+  logger.log('POST /api/orders env:', process.env.NODE_ENV);
 
-    const url = process.env.NODE_ENV === 'development' ? PAYMENT_URL.VPS : PAYMENT_URL.PAY_U;
-    const requestOptions = {
-        method: 'POST',
-        headers: getHeaders(),
-        body: JSON.stringify(getBody()),
-        // redirect: 'follow'
-    };
+  const url = process.env.NODE_ENV === 'development' ? PAYMENT_URL.VPS : PAYMENT_URL.PAY_U;
+  const requestOptions = {
+    method: 'POST',
+    redirect: 'manual',
+    headers: getHeaders(),
+    body: JSON.stringify(getBody()),
+  };
 
-    fetch(url, requestOptions).then(
-        async (response: FetchResponse) => {
-            const resValue = await response.text();
-            logger.log('PayU response:', resValue);
+  fetch(url, requestOptions as RequestInit).then(
+    async (response: FetchResponse) => {
+      const resValue = await response.text();
+      logger.log('PayU response:', resValue);
 
-            res.send(resValue);
-        },
-        (error: FetchError) => {
-            logger.error('PayU error:', error);
+      res.send(resValue);
+    },
+    (error: FetchError) => {
+      logger.error('PayU error:', error);
 
-            res.send(error);
-        }
-    );
+      res.send(error);
+    }
+  );
 });
 
 export default router;
 
 function getBody() {
-    return {
-        "notifyUrl": "https://your.eshop.com/notify",
-        "customerIp": "127.0.0.1",
-        "merchantPosId": "300746",
-        "description": "RTV market",
-        "currencyCode": "PLN",
-        "totalAmount": "21000",
-        "buyer": {
-            "email": "john.doe@example.com",
-            "phone": "654111654",
-            "firstName": "John",
-            "lastName": "Doe",
-            "language": "pl"
-        },
-        "products": [
-            {
-                "name": "Wireless Mouse for Laptop",
-                "unitPrice": "15000",
-                "quantity": "1"
-            },
-            {
-                "name": "HDMI cable",
-                "unitPrice": "6000",
-                "quantity": "1"
-            }
-        ]
-    }
+  return {
+    notifyUrl: 'https://your.eshop.com/notify',
+    customerIp: '127.0.0.1',
+    merchantPosId: '300746',
+    description: 'RTV market',
+    currencyCode: 'PLN',
+    totalAmount: '21000',
+    buyer: {
+      email: 'john.doe@example.com',
+      phone: '654111654',
+      firstName: 'John',
+      lastName: 'Doe',
+      language: 'pl',
+    },
+    products: [
+      {
+        name: 'Wireless Mouse for Laptop',
+        unitPrice: '15000',
+        quantity: '1',
+      },
+      {
+        name: 'HDMI cable',
+        unitPrice: '6000',
+        quantity: '1',
+      },
+    ],
+  };
 }
 
 function getHeaders() {
-    // return {
-    //     Authorization: 'Bearer d9a4536e-62ba-4f60-8017-6053211d3f47',
-    //     'Content-Type': 'application/json',
-    // }
-
-    return {
-        'Content': 'application/json',
-        'Authorization': 'Bearer d9a4536e-62ba-4f60-8017-6053211d3f47',
-        'Content-Type': 'application/json',
-    }
+  return {
+    Content: 'application/json',
+    Authorization: 'Bearer d9a4536e-62ba-4f60-8017-6053211d3f47',
+    'Content-Type': 'application/json',
+  };
 }
-
-// function getReq() {
-//     var myHeaders = new Headers();
-//     myHeaders.append("Content", "application/json");
-//     myHeaders.append("Authorization", "Bearer d9a4536e-62ba-4f60-8017-6053211d3f47");
-//     myHeaders.append("Content-Type", "application/json");
-//
-//     var raw = JSON.stringify({
-//         "notifyUrl": "https://your.eshop.com/notify",
-//         "customerIp": "127.0.0.1",
-//         "merchantPosId": "300746",
-//         "description": "RTV market",
-//         "currencyCode": "PLN",
-//         "totalAmount": "21000",
-//         "buyer": {
-//             "email": "john.doe@example.com",
-//             "phone": "654111654",
-//             "firstName": "John",
-//             "lastName": "Doe",
-//             "language": "pl"
-//         },
-//         "products": [
-//             {
-//                 "name": "Wireless Mouse for Laptop",
-//                 "unitPrice": "15000",
-//                 "quantity": "1"
-//             },
-//             {
-//                 "name": "HDMI cable",
-//                 "unitPrice": "6000",
-//                 "quantity": "1"
-//             }
-//         ]
-//     });
-//
-//     var requestOptions = {
-//         method: 'POST',
-//         headers: myHeaders,
-//         body: raw,
-//         redirect: 'follow'
-//     };
-//
-//     var p = await fetch("http://localhost:3000/api/orders", requestOptions)
-//         .then(response => response.text())
-//         .then(result => logger.log(result))
-//         .catch(error => logger.log('error', error));
-// }
