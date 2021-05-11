253a19f4503115f1201670400f7365a3230e2909
Krzysztof Pawelec <ScriptyChris@gmail.com>
2021-05-11 16:03:56 +0200

    Fix Scroller component element ref being undefined;

--- src/frontend/components/pages/compare.js
+++ src/frontend/components/pages/compare.js
@@ -1,36 +1,47 @@
-import React from 'react';
+import React, { useState, createRef, useEffect } from 'react';
 import appStore from '../../features/appStore';
 import { getProductDetailsData, prepareSpecificProductDetail, getProductDetailsHeaders } from '../views/productDetails';
-// import Scroller from '../utils/scroller';
+import Scroller from '../utils/scroller';
 
 export default function Compare() {
+  const [tableRef] = useState(createRef);
   const productDetailsHeaders = getProductDetailsHeaders();
   const productDetailsHeadersKeys = Object.keys(productDetailsHeaders).filter((header) => header !== 'relatedProducts');
   const comparableProductsData = appStore.productComparisonState.map((product) => getProductDetailsData(product));
 
+  useEffect(() => {
+    tableRef.current.style.setProperty('--compare-rows-number', productDetailsHeadersKeys.length);
+  }, []);
+
   return (
     <section className="compare-products">
-      {/*<Scroller*/}
-      {/*  render={(listRef) => (*/}
-      {/*    <>*/}
-      <table>
-        <thead>
-          {productDetailsHeadersKeys.map((detailHeader, index) => (
-            <tr key={`header-row-${index}`}>
-              <th>{productDetailsHeaders[detailHeader]}</th>
-            </tr>
-          ))}
-        </thead>
-        <tbody>
-          {productDetailsHeadersKeys.map((detailHeader, index) => (
-            <tr key={`body-row-${index}`}>
-              {comparableProductsData.map((productData, i) => (
-                <td key={`cell-${i}`}>{prepareSpecificProductDetail(detailHeader, productData[detailHeader])}</td>
-              ))}
-            </tr>
-          ))}
-        </tbody>
-      </table>
+      <Scroller
+        forwardProps={{ productDetailsHeadersKeys, tableRef }}
+        render={(elementRef, { tableRef }) => {
+          // console.warn('[render] elementRef:', elementRef, ' /curr:', elementRef.current);
+
+          return (
+            <table ref={tableRef} className="compare-products__table scrollable-parent">
+              <thead>
+                {productDetailsHeadersKeys.map((detailHeader, index) => (
+                  <tr key={`header-row-${index}`}>
+                    <th>{productDetailsHeaders[detailHeader]}</th>
+                  </tr>
+                ))}
+              </thead>
+              <tbody ref={elementRef}>
+                {productDetailsHeadersKeys.map((detailHeader, index) => (
+                  <tr key={`body-row-${index}`}>
+                    {comparableProductsData.map((productData, i) => (
+                      <td key={`cell-${i}`}>{prepareSpecificProductDetail(detailHeader, productData[detailHeader])}</td>
+                    ))}
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+          );
+        }}
+      />
     </section>
   );
 }
--- src/frontend/components/utils/scroller.js
+++ src/frontend/components/utils/scroller.js
@@ -13,8 +13,8 @@ function ScrollButton({ directionPointer, handleClick, isVisible, isDisabled, te
   );
 }
 
-export default function Scroller({ render }) {
-  const [listRef, setListRef] = useState(createRef());
+export default function Scroller({ render, forwardProps }) {
+  const [elementRef] = useState(createRef());
   const [scrollingBtnVisible, setScrollingBtnVisible] = useState(false);
   const [leftBtnDisabled, setLeftBtnDisabled] = useState(true);
   const [rightBtnDisabled, setRightBtnDisabled] = useState(false);
@@ -26,42 +26,37 @@ export default function Scroller({ render }) {
   const SCROLL_DIRECTION = { LEFT: 1, RIGHT: -1 };
 
   useEffect(() => {
-    checkIfElementOverflows(true);
     window.addEventListener('resize', checkIfElementOverflows);
-    listRef.current.addEventListener('transitionend', handleElementToParentOffsetChange);
+    elementRef.current.addEventListener('transitionend', handleElementToParentOffsetChange);
+    elementRef.current.dataset.scrollable = 'true';
+
+    checkIfElementOverflows();
 
     return () => {
       window.removeEventListener('resize', checkIfElementOverflows);
-      listRef.current.removeEventListener('transitionend', handleElementToParentOffsetChange);
+      elementRef.current.removeEventListener('transitionend', handleElementToParentOffsetChange);
     };
   }, []);
 
-  useEffect(() => {
-    if (!listRef.current) {
-      setListRef(createRef());
-    }
-  });
-
-  const checkIfElementOverflows = (ignoreHandlingOffset) => {
-    const target = listRef.current.parentNode;
+  const checkIfElementOverflows = (resizeEvent) => {
+    const target = elementRef.current.parentNode;
     const doesElementOverflow = target.clientWidth < target.scrollWidth;
 
     scrollToDirection(null, SCROLL_VARIABLE.LEFT_EDGE);
-    toggleScrollButtons(doesElementOverflow, ignoreHandlingOffset);
+    toggleScrollButtons(doesElementOverflow, resizeEvent);
   };
 
-  const toggleScrollButtons = (showScrollingButtons, ignoreHandlingOffset) => {
+  const toggleScrollButtons = (showScrollingButtons, resizeEvent) => {
     setScrollingBtnVisible(showScrollingButtons);
 
-    if (!ignoreHandlingOffset) {
+    if (resizeEvent) {
       handleElementToParentOffsetChange();
     }
   };
 
   const handleElementToParentOffsetChange = () => {
-    const elementRef = listRef.current;
-    const { left: elementLeftOffset, right: elementRightOffset } = elementRef.getBoundingClientRect();
-    const { left: parentLeftOffset, right: parentRightOffset } = elementRef.parentNode.getBoundingClientRect();
+    const { left: elementLeftOffset, right: elementRightOffset } = elementRef.current.getBoundingClientRect();
+    const { left: parentLeftOffset, right: parentRightOffset } = elementRef.current.parentNode.getBoundingClientRect();
     const leftOffsetGreaterThanParent = Math.round(elementLeftOffset) >= Math.round(parentLeftOffset);
     const rightOffsetLessThanParent = Math.round(elementRightOffset) <= Math.round(parentRightOffset);
 
@@ -71,18 +66,18 @@ export default function Scroller({ render }) {
     if (leftOffsetGreaterThanParent) {
       scrollToDirection(null, SCROLL_VARIABLE.LEFT_EDGE);
     } else if (rightOffsetLessThanParent) {
-      const rightEdge = elementRef.parentNode.offsetWidth - elementRef.offsetWidth;
+      const rightEdge = elementRef.current.parentNode.offsetWidth - elementRef.current.offsetWidth;
       scrollToDirection(null, rightEdge);
     }
   };
 
   const scrollToDirection = (direction, value) => {
-    const oldScrollValue = Number(listRef.current.style.getPropertyValue(SCROLL_VARIABLE.NAME));
+    const oldScrollValue = Number(elementRef.current.style.getPropertyValue(SCROLL_VARIABLE.NAME));
     const newScrollValue = oldScrollValue + SCROLL_VARIABLE.BASE_VALUE * direction;
 
     const scrollValue = direction === null ? value : newScrollValue;
 
-    listRef.current.style.setProperty(SCROLL_VARIABLE.NAME, scrollValue);
+    elementRef.current.style.setProperty(SCROLL_VARIABLE.NAME, scrollValue);
   };
 
   return (
@@ -94,7 +89,7 @@ export default function Scroller({ render }) {
         text={'&larr;'}
         handleClick={scrollToDirection}
       />
-      <div className="scrollable">{render(listRef)}</div>
+      <div>{render(elementRef, forwardProps)}</div>
       <ScrollButton
         directionPointer={SCROLL_DIRECTION.RIGHT}
         isVisible={scrollingBtnVisible}
