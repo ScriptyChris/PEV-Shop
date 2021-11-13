8e1b8d256dfa761caeddc1818cbb7e9716960e52
Krzysztof Pawelec <ScriptyChris@gmail.com>
2021-11-13 23:55:00 +0100

    [WIP] implement responseWrapper, which is going to check correct 'status' and 'data' value according to IEmbracedResponse interface;

--- src/middleware/helpers/middleware-response-wrapper.ts
+++ src/middleware/helpers/middleware-response-wrapper.ts
@@ -1,3 +1,5 @@
+import type { Response } from 'express';
+
 interface IEmbracedResponse {
   // TODO: [REFACTOR] 'authToken' could be always paired with 'payload' prop or be contained by it
   authToken: string | null;
@@ -5,6 +7,67 @@ interface IEmbracedResponse {
   message: string;
   error: string;
   exception: Error | { message: string; stack?: string };
+}
+
+const HTTP_STATUS_CODES = Object.freeze({
+  SUCCESSFUL: {
+    200: 200,
+    201: 201,
+    204: 204,
+  },
+  CLIENT_ERROR: {
+    400: 400,
+    401: 401,
+    403: 403,
+    404: 404,
+    409: 409,
+  },
+  SERVER_ERROR: {
+    500: 500,
+    511: 511,
+  },
+});
+
+type TypeOfHTTPStatusCodes = typeof HTTP_STATUS_CODES;
+
+const mappedHTTPStatusCode = Object.freeze({
+  200: 'SUCCESSFUL',
+  201: 'SUCCESSFUL',
+  204: 'SUCCESSFUL',
+  400: 'CLIENT_ERROR',
+  401: 'CLIENT_ERROR',
+  403: 'CLIENT_ERROR',
+  404: 'CLIENT_ERROR',
+  409: 'CLIENT_ERROR',
+  500: 'SERVER_ERROR',
+  511: 'SERVER_ERROR',
+});
+
+type TSuccessfulHTTPStatusCodesToData = {
+  [SuccessfulStatus in keyof TypeOfHTTPStatusCodes['SUCCESSFUL']]: 'payload' | 'message';
+};
+type TClientErrorHTTPStatusCodesToData = {
+  [ClientErrorStatus in keyof TypeOfHTTPStatusCodes['CLIENT_ERROR']]: 'error';
+};
+type TServerErrorHTTPStatusCodesToData = {
+  [ServerErrorStatus in keyof TypeOfHTTPStatusCodes['SERVER_ERROR']]: 'exception';
+};
+
+type THTTPStatusCodeToData = TSuccessfulHTTPStatusCodesToData &
+  TClientErrorHTTPStatusCodesToData &
+  TServerErrorHTTPStatusCodesToData;
+
+function responseWrapper<
+  Status extends keyof typeof mappedHTTPStatusCode,
+  DataKey extends keyof IEmbracedResponse extends infer K
+    ? K extends keyof IEmbracedResponse
+      ? K extends THTTPStatusCodeToData[Status]
+        ? K
+        : never
+      : never
+    : never
+>(res: Response, status: Status, data: Record<DataKey, IEmbracedResponse[DataKey]>) {
+  return res.status(status).json(data);
 }
 
 export const embraceResponse = <Key extends keyof IEmbracedResponse>(response: Record<Key, IEmbracedResponse[Key]>) =>
