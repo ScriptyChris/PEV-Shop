1dfaf73a31e8f3ec0037d32de741a763d703bcf4
Krzysztof Pawelec <ScriptyChris@gmail.com>
2022-01-27 18:52:08 +0100

    Move docker related files to separate folder; Prepare E2E tests to be run by GitHub CI;

--- test/e2e/run-tests.sh
+++ test/e2e/run-tests.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 
-set -euo pipefail
-
 npx wait-on "${CYPRESS_BASE_URL}"
-npx nodemon \
-  --watch cypress/integration \
-  --watch cypress/support \
-  --exec "prettier --write cypress/integration cypress/support && cypress run --headed"
+
+if [[ "$CYPRESS_TEST_MODE" == "development" ]]
+then
+  npx nodemon \
+    --watch cypress/integration \
+    --watch cypress/support \
+    --exec "prettier --write cypress/integration cypress/support && cypress run"
+else
+  cypress run
+fi
