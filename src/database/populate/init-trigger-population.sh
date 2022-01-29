2eb0bf4881fb27f66fbbdc9cc0c3a210dd5a87aa
Krzysztof Pawelec <ScriptyChris@gmail.com>
2022-01-29 15:28:40 +0100

    Trying to fix GitHub CI E2E: run trigger-population.sh script with sudo;

--- src/database/populate/init-trigger-population.sh
+++ src/database/populate/init-trigger-population.sh
@@ -5,6 +5,6 @@ echo "[<>] population pre init"
 # run script 
 # - as detached (nohup) to prevent being terminated via parent shell 
 # - in background (&) to prevent blocking the container execution
-nohup /home/bin/trigger-population.sh &
+sudo nohup /home/bin/trigger-population.sh &
 
 echo "[<>] population post init"
