ac55344c392daf35f5cfe2de2d27413e13fbf1fc
Krzysztof Pawelec <ScriptyChris@gmail.com>
2022-01-30 21:21:43 +0100

    Trying to fix GitHub CI E2E: fix shell script logging path;

--- src/database/populate/init-trigger-population.sh
+++ src/database/populate/init-trigger-population.sh
@@ -2,9 +2,9 @@
 
 echo "[<>] population pre init"
 
-echo "whoami:"; whoami;
-echo "ls -l /home/bin:"; ls -l /home/bin;
-echo "mod?:"; stat --format '%a' src/database/populate/init-trigger-population.sh;
+echo "whoami -->:"; whoami;
+echo "ls -l /home/bin -->:"; ls -l /home/bin;
+echo "mod? -->:"; stat --format '%a' /home/bin/trigger-population.sh;
 
 # run script 
 # - as detached (nohup) to prevent being terminated via parent shell 
