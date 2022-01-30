e564d48e1056750ff5500d0d5f222f485d25d287
Krzysztof Pawelec <ScriptyChris@gmail.com>
2022-01-30 22:21:08 +0100

    Trying to fix GitHub CI E2E: run 'nohup' via bash;

--- src/database/populate/init-trigger-population.sh
+++ src/database/populate/init-trigger-population.sh
@@ -17,6 +17,9 @@ echo "[0] mod? -->:"; stat --format '%a' /home/bin/trigger-population.sh;
 # - as detached (nohup) to prevent being terminated via parent shell 
 # - in background (&) to prevent blocking the container execution
 ##nohup /home/bin/trigger-population.sh &
-nohup run_script_with_shebang_without_execute_permissions_pollutes_argv /home/bin/trigger-population.sh &
+
+nohup bash /home/bin/trigger-population.sh &
+
+###nohup run_script_with_shebang_without_execute_permissions_pollutes_argv /home/bin/trigger-population.sh &
 
 echo "[<>] population post init"
