fd521ae5f41cf4e2719facc4103f94365a7ce0ce
Krzysztof Pawelec <ScriptyChris@gmail.com>
2022-01-30 21:14:22 +0100

    Trying to fix GitHub CI E2E: log 'mongo' container OS user and some ownership info;

--- src/database/populate/init-trigger-population.sh
+++ src/database/populate/init-trigger-population.sh
@@ -2,9 +2,13 @@
 
 echo "[<>] population pre init"
 
+echo "whoami:"; whoami;
+echo "ls -l /home/bin:"; ls -l /home/bin;
+echo "mod?:"; stat --format '%a' src/database/populate/init-trigger-population.sh;
+
 # run script 
 # - as detached (nohup) to prevent being terminated via parent shell 
 # - in background (&) to prevent blocking the container execution
-sudo nohup /home/bin/trigger-population.sh &
+nohup /home/bin/trigger-population.sh &
 
 echo "[<>] population post init"
