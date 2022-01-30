34d37b07ae8a68e84a2543b508a5aa2732a32202
Krzysztof Pawelec <ScriptyChris@gmail.com>
2022-01-30 22:04:42 +0100

    Trying to fix GitHub CI E2E: trying to avoid 'sudo' issue;

--- src/database/populate/init-trigger-population.sh
+++ src/database/populate/init-trigger-population.sh
@@ -2,6 +2,10 @@
 
 echo "[<>] population pre init"
 
+function run_script_with_shebang_without_execute_permissions_pollutes_argv {
+    $(head -n1 $1 | grep -oP '#!\K.+') $1;
+}
+
 echo "whoami -->:"; whoami;
 echo "ls -l /home/bin -->:"; ls -l /home/bin;
 echo "[0] mod? -->:"; stat --format '%a' /home/bin/trigger-population.sh;
@@ -13,6 +17,7 @@ echo "[1] mod? -->:"; stat --format '%a' /home/bin/trigger-population.sh;
 # run script 
 # - as detached (nohup) to prevent being terminated via parent shell 
 # - in background (&) to prevent blocking the container execution
-nohup /home/bin/trigger-population.sh &
+##nohup /home/bin/trigger-population.sh &
+nohup run_script_with_shebang_without_execute_permissions_pollutes_argv /home/bin/trigger-population.sh &
 
 echo "[<>] population post init"
