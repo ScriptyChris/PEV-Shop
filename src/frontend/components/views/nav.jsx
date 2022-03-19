dd054ca431eaddc91693dab65d3a15799f532114
Krzysztof Pawelec <ScriptyChris@gmail.com>
2022-03-19 17:28:35 +0100

    Refactor nav component to use Material-UI;

--- src/frontend/components/views/nav.jsx
+++ src/frontend/components/views/nav.jsx
@@ -1,7 +1,16 @@
 import { Link, useHistory } from 'react-router-dom';
-import React, { useEffect, useReducer, useState } from 'react';
+import React, { useState } from 'react';
 import { observer } from 'mobx-react-lite';
 import classNames from 'classnames';
+
+import IconButton from '@material-ui/core/IconButton';
+import Menu from '@material-ui/icons/Menu';
+import Drawer from '@material-ui/core/Drawer';
+import ArrowBack from '@material-ui/icons/ArrowBack';
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import MUILink from '@material-ui/core/Link';
+
 import storeService from '@frontend/features/storeService';
 import userSessionService from '@frontend/features/userSessionService';
 import { ROUTES } from '@frontend/components/pages/_routes';
@@ -19,77 +28,63 @@ const translations = Object.freeze({
   account: 'Account',
 });
 
-const navClassNamesReducer = (state, action) => {
-  switch (action.type) {
-    case 'toggleMobile': {
-      return { ...navClassNamesReducer.init, [navClassNamesReducer.classes.isMobile]: action.payload };
-    }
-    case 'toggleMobileExpand': {
-      return {
-        ...state,
-        [navClassNamesReducer.classes.mobileExpanded]: !state[navClassNamesReducer.classes.mobileExpanded],
-      };
-    }
-    default: {
-      throw new TypeError(`Unrecognized action.type: ${action.type}!`);
-    }
-  }
-};
-navClassNamesReducer.init = Object.defineProperties(
-  {},
-  {
-    nav: {
-      value: true,
-      enumerable: true,
-    },
-    toString: {
-      // custom serialization, instead of [object Object]
-      value() {
-        return 'nav';
-      },
-      // be ignored during shallow copy
-      enumerable: false,
-    },
-  }
-);
-navClassNamesReducer.classes = {
-  mobileOverlay: 'nav-mobile-overlay',
-  isMobile: 'nav--is-mobile',
-  mobileExpanded: 'nav--mobile-expanded',
-  navToggleButton: 'nav__toggle-button',
-};
+function NavMenu({ logOutUser, isMobile }) {
+  return (
+    <nav className={classNames('nav', { 'nav--is-mobile': isMobile })}>
+      <List className="nav__links">
+        <ListItem>
+          <MUILink component={Link} to={ROUTES.ROOT}>
+            {translations.start}
+          </MUILink>
+        </ListItem>
+        <ListItem>
+          <MUILink component={Link} to={ROUTES.SHOP}>
+            {translations.shop}
+          </MUILink>
+        </ListItem>
+        <ListItem>
+          <MUILink component={Link} to={ROUTES.ADD_NEW_PRODUCT}>
+            {translations.addNewProduct}
+          </MUILink>
+        </ListItem>
+        <ListItem>
+          <MUILink component={Link} to={ROUTES.MODIFY_PRODUCT}>
+            {translations.modifyProduct}
+          </MUILink>
+        </ListItem>
+        <ListItem>
+          {storeService.userAccountState ? (
+            <MUILink component={Link} to={ROUTES.ROOT} onClick={logOutUser}>
+              {translations.logOut}
+            </MUILink>
+          ) : (
+            <MUILink component={Link} to={ROUTES.LOG_IN} data-cy={`link:${ROUTES.LOG_IN}`}>
+              {translations.logIn}
+            </MUILink>
+          )}
+        </ListItem>
+        <ListItem>
+          {storeService.userAccountState ? (
+            <MUILink component={Link} to={ROUTES.ACCOUNT}>
+              {translations.account}
+            </MUILink>
+          ) : (
+            <MUILink component={Link} to={ROUTES.REGISTER} data-cy={`link:${ROUTES.REGISTER}`}>
+              {translations.register}
+            </MUILink>
+          )}
+        </ListItem>
+      </List>
+    </nav>
+  );
+}
 
 export default observer(function Nav() {
   const history = useHistory();
   const isMobileLayout = useMobileLayout();
-  const [navClassNames, dispatchNavClassNames] = useReducer(navClassNamesReducer, navClassNamesReducer.init);
-  const [navOverlayClasses, setNavOverlayClasses] = useState(navClassNamesReducer.classes.mobileOverlay);
-  const [navToggleButtonClasses, setNavToggleButtonClasses] = useState(navClassNamesReducer.classes.navToggleButton);
+  const [isMobileMenuOpened, setIsMobileMenuOpened] = useState(false);
 
-  useEffect(() => {
-    dispatchNavClassNames({
-      type: 'toggleMobile',
-      payload: isMobileLayout,
-    });
-  }, [isMobileLayout]);
-
-  useEffect(() => {
-    const EMPTY_CLASS_NAME = '';
-    const isNavExpanded = navClassNames[navClassNamesReducer.classes.mobileExpanded];
-
-    setNavOverlayClasses(isNavExpanded ? navClassNamesReducer.classes.mobileOverlay : EMPTY_CLASS_NAME);
-    setNavToggleButtonClasses(
-      classNames(navClassNamesReducer.classes.navToggleButton, {
-        [`${navClassNamesReducer.classes.navToggleButton}--mobile-hidden`]: !isNavExpanded,
-      })
-    );
-  }, [navClassNames]);
-
-  const handleToggleNavMenu = () => {
-    dispatchNavClassNames({
-      type: 'toggleMobileExpand',
-    });
-  };
+  const handleToggleNavMenu = () => setIsMobileMenuOpened(!isMobileMenuOpened);
 
   const handleNavMobileOverlayClick = (event) => {
     const clickedInOverlay = event.target === event.currentTarget;
@@ -111,49 +106,33 @@ export default observer(function Nav() {
   };
 
   return (
-    <div onClick={handleNavMobileOverlayClick} className={navOverlayClasses}>
-      {isMobileLayout && (
-        <button onClick={handleToggleNavMenu} className={navToggleButtonClasses}>
-          {translations.toggleNavMenu}
-        </button>
-      )}
+    <div onClick={handleNavMobileOverlayClick}>
+      {isMobileLayout ? (
+        <>
+          <IconButton
+            onClick={handleToggleNavMenu}
+            className="nav__toggle-button"
+            aria-label={translations.toggleNavMenu}
+            title={translations.toggleNavMenu}
+          >
+            <Menu />
+          </IconButton>
 
-      <nav className={classNames(navClassNames)}>
-        <ul className={`${navClassNamesReducer.init}__links`}>
-          <li>
-            <Link to={ROUTES.ROOT}>{translations.start}</Link>
-          </li>
-          <li>
-            <Link to={ROUTES.SHOP}>{translations.shop}</Link>
-          </li>
-          <li>
-            <Link to={ROUTES.ADD_NEW_PRODUCT}>{translations.addNewProduct}</Link>
-          </li>
-          <li>
-            <Link to={ROUTES.MODIFY_PRODUCT}>{translations.modifyProduct}</Link>
-          </li>
-          <li>
-            {storeService.userAccountState ? (
-              <Link to={ROUTES.ROOT} onClick={logOutUser}>
-                {translations.logOut}
-              </Link>
-            ) : (
-              <Link to={ROUTES.LOG_IN} data-cy={`link:${ROUTES.LOG_IN}`}>
-                {translations.logIn}
-              </Link>
-            )}
-          </li>
-          <li>
-            {storeService.userAccountState ? (
-              <Link to={ROUTES.ACCOUNT}>{translations.account}</Link>
-            ) : (
-              <Link to={ROUTES.REGISTER} data-cy={`link:${ROUTES.REGISTER}`}>
-                {translations.register}
-              </Link>
-            )}
-          </li>
-        </ul>
-      </nav>
+          <Drawer anchor="left" open={isMobileMenuOpened} onClose={handleToggleNavMenu}>
+            <IconButton
+              onClick={handleToggleNavMenu}
+              aria-label={translations.toggleNavMenu}
+              title={translations.toggleNavMenu}
+            >
+              <ArrowBack />
+            </IconButton>
+
+            <NavMenu logOutUser={logOutUser} isMobile={true} />
+          </Drawer>
+        </>
+      ) : (
+        <NavMenu logOutUser={logOutUser} />
+      )}
     </div>
   );
 });
