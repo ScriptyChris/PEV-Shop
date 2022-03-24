f48ff5446ccad1ae458bf1f7327c09444023cea0
Krzysztof Pawelec <ScriptyChris@gmail.com>
2022-03-24 09:07:40 +0100

    Add scroll to top button;

---
+++ src/frontend/components/utils/scrollToTop.jsx
@@ -0,0 +1,42 @@
+import React, { useState, useEffect } from 'react';
+
+import Zoom from '@material-ui/core/Zoom';
+import KeyboardArrowUpIcon from '@material-ui/icons/KeyboardArrowUp';
+import Fab from '@material-ui/core/Fab';
+
+import { useMobileLayout } from '@frontend/contexts/mobile-layout';
+
+function ScrollToTop() {
+  const isMobileLayout = useMobileLayout();
+  const [isScrollBtnVisible, setIsScrollBtnVisible] = useState(false);
+
+  useEffect(() => {
+    document.addEventListener('scroll', toggleScrollBtnVisibility, { passive: true });
+
+    return () => document.removeEventListener('scroll', toggleScrollBtnVisibility, { passive: true });
+  }, []);
+
+  const toggleScrollBtnVisibility = () => setIsScrollBtnVisible(window.scrollY > 0);
+
+  const handleScrollToTop = () => {
+    window.scrollTo({
+        top: 0,
+        behavior: 'smooth',
+    });
+  };
+
+  return (
+    <Zoom in={isScrollBtnVisible}>
+      <Fab
+        onClick={handleScrollToTop}
+        color="primary"
+        className="scroll-to-top-btn"
+        size={isMobileLayout ? 'small' : 'large'}
+      >
+        <KeyboardArrowUpIcon />
+      </Fab>
+    </Zoom>
+  );
+}
+
+export { ScrollToTop };
